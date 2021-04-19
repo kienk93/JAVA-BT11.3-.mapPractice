85d398fe6573ede718d2f04c7dc646a15c6d280a
kienk93 <kienk93@users.noreply.github.com>
2021-04-19 13:53:24 +0700

    'add'

---
+++ KhoiTapTreeMap.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class KhoiTapTreeMap {
+    public static void main(String[] args) {
+        String st = "asbvbcddd";
+        HashMap<Character, Integer> map1 = new HashMap<>();
+
+        for (int i=0;i<st.length();i++){
+            if (map1.containsKey(st.charAt(i))){
+                map1.replace(st.charAt(i), map1.get(st.charAt(i))+1);
+            } else {
+                map1.put(st.charAt(i), 1);
+            }
+        }
+        System.out.println(map1);
+    }
+}
